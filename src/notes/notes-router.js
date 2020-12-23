77ab91f84d984a97b07370cecb6f3a8eb011499c
Josephine Han <jsphnhan@gmail.com>
2020-12-22 18:56:01 -0800

    changes notes_id to note_id in note router

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -1,97 +1,81 @@
+const path = require("path");
+const express = require("express");
+const NotesService = require("./notes-service");
 
-const path = require('path')
-const express = require('express')
-const NotesService = require('./notes-service')
-
-const notesRouter = express.Router()
-const jsonParser = express.json()
-
-notesRouter
-    .route('/')
-    .get((req, res, next) => {
-    NotesService.getAllNotes(
-        req.app.get('db')
-    )
-        .then(notes => {
-            res.json(notes)
-        })
-        .catch(next)
-    })
-
-    .post(jsonParser, (req, res, next) => {
-        const { name, date_modified, folder_id, content} = req.body
-        const newNote = { name, date_modified, folder_id, content }
-
-        for(const [key, value] of Object.entries(newNote)) {
-            if (value === null) {
-                return res.status(400).json({
-                    error: { message: `Missing '${key}' in request body`}
-                })
-            }
-        }
-
-        NotesService.insertNote(
-            req.app.get('db'),
-            newNote
-        )
-            .then(note => {
-                res
-                    .status(201)
-                    .location(path.posix.join(req.originalUrl, `/${note.id}`))
-                    .json(note)
-            })
-            .catch(next)
-    })
+const notesRouter = express.Router();
+const jsonParser = express.json();
 
 notesRouter
-    .route('/:notes_id')
-    .all((req, res, next) => {
-        NotesService.getById(
-            req.app.get('db'),
-            req.params.note_id
-        )
-            .then(note => {
-                if (!note) {
-                    return res.status(404).json({
-                        error: { message: `Note doesn't exist` }
-                    })
-                }
-                res.json(note)
-            })
-            .catch(next)
-    })
-    .delete((req, res, next) => {
-        NotesService.deleteNote(
-            req.app.get('db'),
-            req.params.note_id
-        )
-            .then(() => {
-                res.status(204).end()
-            })
-            .catch(next)
-    })
-    .patch(jsonParser, (req, res, next) => {
-        const { name, date_modified, folder_id, content} = req.body
-        const noteToUpdate = { name, date_modified, folder_id, content }
+  .route("/")
+  .get((req, res, next) => {
+    NotesService.getAllNotes(req.app.get("db"))
+      .then((notes) => {
+        res.json(notes);
+      })
+      .catch(next);
+  })
 
-        const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
-        if (numberOfValues === 0) {
-            return res.status(400).json({
-                error: {
-                    message: `Request body most contain either 'name', 'date_modified', 'folder_id', 'content'`
-                }
-            })
+  .post(jsonParser, (req, res, next) => {
+    const { name, date_modified, folder_id, content } = req.body;
+    const newNote = { name, date_modified, folder_id, content };
+
+    for (const [key, value] of Object.entries(newNote)) {
+      if (value === null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+      }
+    }
+
+    NotesService.insertNote(req.app.get("db"), newNote)
+      .then((note) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(note);
+      })
+      .catch(next);
+  });
+
+notesRouter
+  .route("/:note_id")
+  .all((req, res, next) => {
+    NotesService.getById(req.app.get("db"), req.params.note_id)
+      .then((note) => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` },
+          });
         }
-        
-        NotesService.updateNote(
-            req.app.get('db'),
-            req.params.note_id,
-            noteToUpdate
-        )
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-            .catch(next)
-    })  
+        res.json(note);
+      })
+      .catch(next);
+  })
+  .delete((req, res, next) => {
+    NotesService.deleteNote(req.app.get("db"), req.params.note_id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { name, date_modified, folder_id, content } = req.body;
+    const noteToUpdate = { name, date_modified, folder_id, content };
 
-module.exports = notesRouter
+    const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body most contain either 'name', 'date_modified', 'folder_id', 'content'`,
+        },
+      });
+    }
+
+    NotesService.updateNote(req.app.get("db"), req.params.note_id, noteToUpdate)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = notesRouter;
