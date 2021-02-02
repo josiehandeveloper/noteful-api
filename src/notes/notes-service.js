e13987148df70a25a18aee2335ad6989f5458222
Josephine Han <jsphnhan@gmail.com>
2021-02-02 13:55:56 -0800

    notes-service

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -1,29 +1,25 @@
 const NotesService = {
-    getAllNotes(knex) {
-        return knex.select('*').from('notes')
-    },
-    getById(knex, id) {
-        return knex.from('notes').select('*').where('id', id).first()
-    },
-    insertNote(knex, newNote) {
-        return knex
-            .insert(newNote)
-            .into('notes')
-            .returning('*')
-            .then(rows => {
-                return rows[0]
-            })
-    },
-    deleteNote(knex, id) {
-        return knex('notes')
-        .where({id})
-        .delete()
-    },
-    updateNote(knex, id, newNoteFields) {
-        return knex('notes')
-          .where({ id })
-          .update(newNoteFields)
-    },
-}
+  getAllNotes(knex) {
+    return knex.select("*").from("notes");
+  },
+  getById(knex, id) {
+    return knex.from("notes").select("*").where("id", id).first();
+  },
+  insertNote(knex, newNote) {
+    return knex
+      .insert(newNote)
+      .into("notes")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  deleteNote(knex, id) {
+    return knex("notes").where({ id }).delete();
+  },
+  updateNote(knex, id, newNoteFields) {
+    return knex("notes").where("id", id).update(newNoteFields);
+  },
+};
 
-module.exports = NotesService
+module.exports = NotesService;
